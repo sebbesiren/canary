a7875ec1220337c951e898c8da07a87cded4bc01
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2023-11-18 20:22:29 +0100

    update: removed demand for 4 players and check for people has not done it

--- data-canary/scripts/actions/quests/annihilator.lua
+++ data-canary/scripts/actions/quests/annihilator.lua
@@ -18,7 +18,7 @@ function annihilator.onUse(player, item, fromPosition, target, toPosition, isHot
 		local players = {}
 		for _, position in ipairs(playerPosition) do
 			local topPlayer = Tile(position):getTopCreature()
-			if not topPlayer or not topPlayer:isPlayer() or topPlayer:getLevel() < 100 or topPlayer:getStorageValue(Storage.Quest.ExampleQuest.Example) ~= -1 then
+			if topPlayer:getLevel() < 100  then
 				player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
 				return false
 			end
