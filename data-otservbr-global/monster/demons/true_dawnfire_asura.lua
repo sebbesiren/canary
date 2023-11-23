8a05cf3a62dd2eae29ee52d5d9ab551e0008c277
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2023-11-23 22:26:46 +0100

    tuned exp in asura mirror

--- data-otservbr-global/monster/demons/true_dawnfire_asura.lua
+++ data-otservbr-global/monster/demons/true_dawnfire_asura.lua
@@ -2,7 +2,7 @@ local mType = Game.createMonsterType("True Dawnfire Asura")
 local monster = {}
 
 monster.description = "a true dawnfire asura"
-monster.experience = 13750
+monster.experience = 11500
 monster.outfit = {
 	lookType = 1068,
 	lookHead = 114,
