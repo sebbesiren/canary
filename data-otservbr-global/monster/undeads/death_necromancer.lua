026cd549d837cb905f7f3f149b58100a9aab9f02
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2023-11-30 20:36:42 +0100

    update: death area

--- data-otservbr-global/monster/undeads/death_necromancer.lua
+++ data-otservbr-global/monster/undeads/death_necromancer.lua
@@ -29,7 +29,7 @@ monster.Bestiary = {
 
 monster.health = 10000
 monster.maxHealth = monster.health
-monster.experience = monster.health * 1.8
+monster.experience = monster.health * 1.5
 
 monster.race = "blood"
 monster.corpse = 28861
