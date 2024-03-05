be8915ff59a542ffd5f0ac57ce1c624cf80b3552
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2024-03-05 22:16:04 +0100

    update: pohu npc

--- data-otservbr-global/npc/pohusaxi.lua
+++ data-otservbr-global/npc/pohusaxi.lua
@@ -1102,6 +1102,7 @@ npcConfig.shop = {
 	{ itemName = "giant amethyst", clientId = 30061, sell = 60000 },
 	{ itemName = "fire mushroom", clientId = 3731, sell = 200 },
 	{ itemName = "amulet of loss", clientId = 3057, buy = 50000, sell = 45000 },
+	{ itemName = "nighthunter wing", clientId = 39381, sell = 2000 },
 }
 -- On buy npc shop message
 npcType.onBuyItem = function(npc, player, itemId, subType, amount, ignore, inBackpacks, totalCost)
@@ -1112,7 +1113,8 @@ npcType.onSellItem = function(npc, player, itemId, subtype, amount, ignore, name
 	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Sold %ix %s for %i gold.", amount, name, totalCost))
 end
 -- On check npc shop message (look item)
-npcType.onCheckItem = function(npc, player, clientId, subType) end
+npcType.onCheckItem = function(npc, player, clientId, subType)
+end
 
 local function creatureSayCallback(npc, creature, type, message)
 	local player = Player(creature)
