f311091a6878faddbb17dde4b69e5d22aa5c9243
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2024-03-16 12:15:37 +0100

    update: rebalance exp

--- data-otservbr-global/monster/demons/grimeleech.lua
+++ data-otservbr-global/monster/demons/grimeleech.lua
@@ -2,7 +2,7 @@ local mType = Game.createMonsterType("Grimeleech")
 local monster = {}
 
 monster.description = "a grimeleech"
-monster.experience = 12000
+monster.experience = 13500
 monster.outfit = {
 	lookType = 855,
 	lookHead = 0,
