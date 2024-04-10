fca82681e0a6e540135e98c01811e1a1b91df9fd
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2024-04-10 22:33:04 +0200

    feat: soulwars hazard

---
+++ data-otservbr-global/scripts/hazard/custom/soulwars.lua
@@ -0,0 +1,49 @@
+local hazard = Hazard.new({
+	name = "hazard.soulwars",
+	from = Position({x = 33873, y = 30994, z = 9}),
+	to = Position(34084, 31253, 13),
+	maxLevel = 20,
+
+	crit = true,
+	dodge = true,
+	damageBoost = true,
+	defenseBoost = true,
+})
+
+hazard:register()
+
+local hazardZone = Zone.getByName(hazard.name)
+hazardZone:addArea({x = 33796, y = 31813, z = 3}, {x = 33916, y = 31930, z = 5})
+
+local deathEventName = "SoulwarsDeath"
+local spawnEvent = ZoneEvent(hazardZone)
+function spawnEvent.onSpawn(monster, position)
+	monster:registerEvent(deathEventName)
+end
+spawnEvent:register()
+
+local deathEvent = CreatureEvent(deathEventName)
+function deathEvent.onDeath(creature)
+	local monster = creature:getMonster()
+	if not creature or not monster or not monster:hazard() or not hazard:isInZone(monster:getPosition()) then
+		return true
+	end
+
+	local player, points = hazard:getHazardPlayerAndPoints(monster:getDamageMap())
+	if points < 1 then
+		return true
+	end
+
+	if executeLevelUpEvent(points) then
+		onDeathForDamagingPlayers(creature, function(creature, damagingPlayer)
+			attemptLevelUpPlayer(hazard, damagingPlayer, points)
+		end)
+	end
+
+	if executeCreateHazardPod(points) then
+		createHazardPod(monster:getPosition(), monster:getName())
+	end
+
+	return true
+end
+deathEvent:register()
