d733f70b21170142d200a86703533ded4de463f0
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2025-03-01 17:56:51 +0100

    update: remove drunk from asura

--- data-otservbr-global/monster/demons/midnight_asura.lua
+++ data-otservbr-global/monster/demons/midnight_asura.lua
@@ -118,7 +118,7 @@ monster.attacks = {
 	{ name = "combat", interval = 4100, chance = 27, type = COMBAT_DEATHDAMAGE, minDamage = -150, maxDamage = -300, length = 8, spread = 0, effect = CONST_ME_MORTAREA, target = false },
 	{ name = "combat", interval = 2700, chance = 15, type = COMBAT_DEATHDAMAGE, minDamage = -50, maxDamage = -200, range = 5, shootEffect = CONST_ANI_SUDDENDEATH, target = true },
 	{ name = "combat", interval = 3100, chance = 15, type = COMBAT_ENERGYDAMAGE, minDamage = -50, maxDamage = -100, range = 1, shootEffect = CONST_ANI_ENERGY, target = true },
-	{ name = "drunk", interval = 2000, chance = 10, range = 7, shootEffect = CONST_ANI_ENERGY, target = false, duration = 5000 },
+	--{ name = "drunk", interval = 2000, chance = 10, range = 7, shootEffect = CONST_ANI_ENERGY, target = false, duration = 5000 },
 }
 
 monster.defenses = {
