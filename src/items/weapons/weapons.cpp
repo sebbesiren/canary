94088fba60d234ade2b370a369d9bac98f1fb9ba
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2025-03-05 21:14:26 +0100

    fix: remove more level req for hero/legendary vocs

--- src/items/weapons/weapons.cpp
+++ src/items/weapons/weapons.cpp
@@ -154,7 +154,8 @@ int32_t Weapon::playerWeaponCheck(const std::shared_ptr<Player> &player, const s
 		}
 
 		int32_t damageModifier = 100;
-		if (player->getLevel() < getReqLevel()) {
+		// Remove level check for heroic/legendary vocations
+		if (player->getLevel() < getReqLevel()  && player->getVocationId() < 20) {
 			damageModifier = (isWieldedUnproperly() ? damageModifier / 2 : 0);
 		}
 
