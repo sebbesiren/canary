8f672383f644d2cc703bff5e98635d96a659e9de
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2025-03-23 01:26:59 +0100

    update: rvert a change

--- data-otservbr-global/lib/quests/soul_war.lua
+++ data-otservbr-global/lib/quests/soul_war.lua
@@ -30,7 +30,7 @@ SoulWarQuest = {
 
 	timeToReturnImmuneMegalomania = 70, -- In seconds
 
-	bagYouDesireChancePerTaint = 10, -- Increases % per taint
+	bagYouDesireChancePerTaint = 15, -- Increases % per taint
 	bagYouDesireMonsters = {
 		"Bony Sea Devil",
 		"Brachiodemon",
